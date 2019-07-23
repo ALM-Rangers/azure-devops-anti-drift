ce95214b257b275b4ce322f36fa3c008368017ad
Mario Majcica <mario@majcica.com>
2019-07-23 09:00:23 +0200

    Added unit test project. MsTest and Moq.

---
+++ src/drift.tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Rangers.Antidrift.Drift.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestOfTheTests()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
