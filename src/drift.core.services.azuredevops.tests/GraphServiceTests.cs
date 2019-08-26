8acafcfbe2467339b337b64545f8aba5f8ecfed7
Vladimir Gusarov <vgusarov@hotmail.com>
2019-08-27 01:00:10 +0300

    Fixed StyleCop settings file inclusion. Corrected file header.

--- src/drift.core.services.azuredevops.tests/GraphServiceTests.cs
+++ src/drift.core.services.azuredevops.tests/GraphServiceTests.cs
@@ -1,6 +1,12 @@
-// <copyright file="GraphServiceTests.cs" company="PlaceholderCompany">
-// Copyright (c) PlaceholderCompany. All rights reserved.
+// -----------------------------------------------------------------------
+// <copyright file="GraphServiceTests.cs" company="ALM | DevOps Rangers">
+//    This code is licensed under the MIT License.
+//    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF
+//    ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+//    TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR
+//    A PARTICULAR PURPOSE AND NONINFRINGEMENT.
 // </copyright>
+// -----------------------------------------------------------------------
 
 namespace Rangers.Antidrift.Drift.Core.Services.AzureDevOps.Tests
 {
