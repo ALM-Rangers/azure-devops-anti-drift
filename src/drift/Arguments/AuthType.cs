00e51aa4b9106a66aa86e4f53d36fe67d392d342
Vladimir Gusarov <vgusarov@hotmail.com>
2019-09-02 21:39:22 +0300

    Added XML docs to AuthType enum

--- src/drift/Arguments/AuthType.cs
+++ src/drift/Arguments/AuthType.cs
@@ -10,11 +10,29 @@
 
 namespace Rangers.Antidrift.Drift.Arguments
 {
+    /// <summary>
+    /// Defines Azure DevOps authentication types
+    /// </summary>
     public enum AuthType
     {
+        /// <summary>
+        /// Use personal access token
+        /// </summary>
         Pat,
+
+        /// <summary>
+        /// Use basic authentication
+        /// </summary>
         Basic,
+
+        /// <summary>
+        /// Use NTLM authentication
+        /// </summary>
         Ntlm,
+
+        /// <summary>
+        /// Use intercative authentication prompt like in Visual Studio
+        /// </summary>
         Interactive,
     }
 }
