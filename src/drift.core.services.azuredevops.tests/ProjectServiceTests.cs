8fc5857a5cfb87e8603e04ea9ae5f8745a76a9ee
Freek Giele <freek.giele@gmail.com>
2020-02-21 08:29:09 +0100

    Added the project service

---
+++ src/drift.core.services.azuredevops.tests/ProjectServiceTests.cs
@@ -0,0 +1,61 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="ProjectCollectionServiceTests.cs" company="ALM | DevOps Rangers">
+//    This code is licensed under the MIT License.
+//    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF
+//    ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+//    TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR
+//    A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Rangers.Antidrift.Drift.Core.Services.AzureDevOps.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using FluentAssertions;
+    using Microsoft.VisualStudio.Services.Common;
+    using Microsoft.VisualStudio.Services.WebApi;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class ProjectServiceTests
+    {
+        public TestContext TestContext { get; set; }
+
+        public string Organization
+        {
+            get { return this.TestContext.Properties["organization"].ToString(); }
+        }
+
+        public string PAT
+        {
+            get { return this.TestContext.Properties["pat"].ToString(); }
+        }
+
+        private Guid Antidrift => new Guid(this.TestContext.Properties["antidriftprojectguid"].ToString());
+
+        [TestMethod]
+        public async Task GetProjects()
+        {
+            // Arrange
+            var credentials = new VssBasicCredential(string.Empty, this.PAT);
+            var url = $"https://dev.azure.com/{this.Organization}";
+            var connection = new VssConnection(new Uri(url), credentials);
+
+            var expected = new TeamProject { Id = this.Antidrift, Name = "Antidrift" };
+
+            var target = new ProjectService(connection);
+
+            // Act
+            var actual = await target.GetProjects().ConfigureAwait(false);
+
+            // Assert
+            actual.Should().ContainEquivalentOf(expected);
+
+            connection.Dispose();
+            await Task.CompletedTask.ConfigureAwait(false);
+        }
+    }
+}
---
+++ src/drift.core.services.azuredevops/ProjectService.cs
@@ -0,0 +1,44 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="ProjectService.cs" company="ALM | DevOps Rangers">
+//    This code is licensed under the MIT License.
+//    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF
+//    ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+//    TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR
+//    A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Rangers.Antidrift.Drift.Core.Services
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Microsoft.TeamFoundation.Core.WebApi;
+    using Microsoft.VisualStudio.Services.WebApi;
+
+    public class ProjectService
+    {
+        private readonly VssConnection connection;
+
+        public ProjectService(VssConnection connection)
+        {
+            this.connection = connection;
+        }
+
+        public async Task<IEnumerable<Core.TeamProject>> GetProjects()
+        {
+            var client = this.connection.GetClient<ProjectHttpClient>();
+
+            var projectsInPage = await client.GetProjects(top: 250).ConfigureAwait(false);
+            var projects = projectsInPage.ToList();
+
+            while (!string.IsNullOrWhiteSpace(projectsInPage.ContinuationToken))
+            {
+                projectsInPage = await client.GetProjects(continuationToken: projectsInPage.ContinuationToken).ConfigureAwait(false);
+                projects.AddRange(projectsInPage.ToArray());
+            }
+
+            return projects.Select(tp => new Core.TeamProject { Id = tp.Id, Name = tp.Name });
+        }
+    }
+}
